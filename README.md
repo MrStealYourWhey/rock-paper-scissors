5c0ca4f382faf95b0b35ae4b15a9fe382b4181ea
mrstealyourwhey <fdewfdew@gmail.com>
2020-09-07 13:09:31 +0200

    .

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 Exercise from The Odin Project.
-Simple rock, paper, scissors game for the web console.
-Program asks user for how many rounds he wishes to play, updates the score after each round
-and declares the winner at the end
+Simple rock, paper, scissors game
+
---
+++ game.js
@@ -0,0 +1,61 @@
+play();
+
+let computerScore = 0
+let userScore = 0
+function game (userSelection)
+{
+    let choices = ["rock", "paper", "scissors"];
+    function computerPlay()
+    {
+        let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+        if (computerChoice === choices[0]){
+                return "Rock";
+        }
+        else if (computerChoice === choices[1]){
+            return "Paper";
+        }
+        else{
+            return "Scissors";
+        }
+    }
+    let computerSelection = computerPlay()
+    function playRound(computerSelection)
+    {
+        if (computerSelection === userSelection){
+            document.getElementById("result").innerHTML = ("Draw")
+        }
+        else if ((computerSelection === "Rock" && userSelection === "Scissors") || (computerSelection === "Paper" && userSelection === "Rock") || (computerSelection === "Scissors" && userSelection === "Paper")){
+            document.getElementById("result").innerHTML = ("You lose! " + computerSelection + " beats " + userSelection + ".")
+            computerScore++
+        }
+        else{
+            document.getElementById("result").innerHTML = ("You win! " + userSelection + " beats " + computerSelection + ".")
+            userScore++
+        }
+    document.getElementById("uscore").innerHTML = userScore;
+    document.getElementById("cscore").innerHTML = computerScore;   
+    }
+    playRound(computerSelection)   
+}
+
+function play () {
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let userSelection = button.innerHTML;
+            game(userSelection);
+        })
+    })
+}
+
+rscore.addEventListener('click', reset);
+
+function reset() {
+    userScore = 0;
+    computerScore = 0;
+    document.getElementById("uscore").innerHTML = userScore;
+    document.getElementById("cscore").innerHTML = computerScore; 
+    document.getElementById("result").innerHTML = "";
+}
+
+
